e425133799511396641373efd89d8af990138f3d
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-09-10 04:15:51 -0400

    Update script.js

--- backflip/js/script.js
+++ backflip/js/script.js
@@ -1,25 +1,19 @@
 
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
-
 const gif_1 = "https://jcamille2023.github.io/jcamille2023/backflip/backflip-its-a-man-doing-a-backflip.gif"
 const time = 2.3
 const photo_1 = "https://jcamille2023.github.io/jcamille2023/backflip/ezgif-1-6edddceba4.png"
 const gif_2 = "https://jcamille2023.github.io/jcamille2023/backflip/ezgif-1-6edddceba4.gif"
 const photo_2 = "https://jcamille2023.github.io/jcamille2023/backflip/backflip-its-a-man-doing-a-backflip.png"
 
+function wait() {
+    console.log(".")
+}
 
-setTimeout(function() {
-
-console.log(".");
-
-}, 2300);
 
 function do_a_flip() {
     var photo = document.getElementById("photo")
     photo.setAttribute("src",gif_1)
-    setTimeout()
+    setTimeout(wait(),2300)
     photo.setAttribute("src",photo_1)
     
 

b452576ecc24483994d0be1d12f9161cdfe2d487
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-09-14 19:18:29 -0400

    Update script.js
    Changed link to reference new domain

--- backflip/js/script.js
+++ backflip/js/script.js
@@ -1,9 +1,9 @@
 
-const gif_1 = "https://jcamille2023.github.io/jcamille2023/backflip/backflip-its-a-man-doing-a-backflip.gif"
+const gif_1 = "https://jcamille2023.github.io/backflip/backflip-its-a-man-doing-a-backflip.gif"
 const time = 2.3
-const photo_1 = "https://jcamille2023.github.io/jcamille2023/backflip/ezgif-1-6edddceba4.png"
-const gif_2 = "https://jcamille2023.github.io/jcamille2023/backflip/ezgif-1-6edddceba4.gif"
-const photo_2 = "https://jcamille2023.github.io/jcamille2023/backflip/backflip-its-a-man-doing-a-backflip.png"
+const photo_1 = "https://jcamille2023.github.io/backflip/ezgif-1-6edddceba4.png"
+const gif_2 = "https://jcamille2023.github.io/backflip/ezgif-1-6edddceba4.gif"
+const photo_2 = "https://jcamille2023.github.io/backflip/backflip-its-a-man-doing-a-backflip.png"
 
 function wait() {
     console.log(".")

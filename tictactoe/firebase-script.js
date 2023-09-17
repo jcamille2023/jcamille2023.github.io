9a889b628780dd19fbd715ef11f36e07b0d7f41d
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-09-17 00:07:08 -0400

    Update firebase-script.js

--- tictactoe/firebase-script.js
+++ tictactoe/firebase-script.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
   import { getAuth, onAuthStateChanged, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+  import { getDatabase, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 

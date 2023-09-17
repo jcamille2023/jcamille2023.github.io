b9761b94106ccb85db359ebae2bd1ecff29bd4eb
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-09-16 22:06:08 -0400

    Update script-multiplayer.js
    Added automatic anonymous sign-in to Firebase

--- tictactoe/script-multiplayer.js
+++ tictactoe/script-multiplayer.js
@@ -166,18 +166,51 @@ function move(a) {
     
   
 }
-onAuthStateChanged(auth, (user) => {
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getAuth, onAuthStateChanged, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkf2Xme8vIYwSjNgpikMPlHETkteqEsfI",
+    authDomain: "tic-tac-toe-online-108ca.firebaseapp.com",
+    databaseURL: "https://tic-tac-toe-online-108ca-default-rtdb.firebaseio.com",
+    projectId: "tic-tac-toe-online-108ca",
+    storageBucket: "tic-tac-toe-online-108ca.appspot.com",
+    messagingSenderId: "971654471519",
+    appId: "1:971654471519:web:3e05a829c5db81a4f920ea"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/auth.user
     const uid = user.uid;
+    console.log("User is signed in");
+    console.log(uid);
     // ...
   } else {
+    console.log("User is signed out");
     // User is signed out
-    console.log("User is logged out.")
     // ...
   }
 });
+
+signInAnonymously(auth)
+  .then(() => {
+    // Signed in..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
 
 
   

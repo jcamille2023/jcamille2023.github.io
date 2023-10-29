54c2b0418fb39ba76c43db65bf88cf2d7270fc42
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-10-28 21:22:12 -0400

    Create dashboard.js

---
+++ insight/dashboard.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { getDatabase, set, ref, onValue, get, child } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyCqUDOyX-OrrrncNv5uABW8hiLndPsMDMg",
+    authDomain: "insight-34bc8.firebaseapp.com",
+    projectId: "insight-34bc8",
+    storageBucket: "insight-34bc8.appspot.com",
+    messagingSenderId: "652635905647",
+    appId: "1:652635905647:web:05a5378b1f51f58a6d779a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+function logout() {
+  signOut(auth).then(() => {
+  console.log("User is signed out.");
+  window.location.href = "index.html";
+  }).catch((error) => {
+  // An error happened.
+  });
+}
+window.logout = logout;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    console.log(user);
+    const uid = user.uid;
+    document.getElementById("username").innerHTML = user.displayName;
+    document.getElementById("user-greeting").innerHTML = "Hi, " + user.displayName + "!";
+    
+    // ...
+  } else {
+    document.getElementById("sign-in-button").setAttribute("onclick","login()");
+    window.location.href = "index.html";
+    // ...
+  }
+});
+

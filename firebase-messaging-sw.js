f1c3d76baba8fde4b5decf902d7496e342837e86
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-25 23:50:16 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,34 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.1/firebase-app.js";
-import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-sw.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js';
+import { getMessaging, onBackgroundMessage } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-sw.js';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyC5oq9fyPeoo8jVU-N07gYhjt2kFEBGqA8",
-    authDomain: "arc-by-insight.firebaseapp.com",
-    databaseURL: "https://arc-by-insight-default-rtdb.firebaseio.com",
-    projectId: "arc-by-insight",
-    storageBucket: "arc-by-insight.appspot.com",
-    messagingSenderId: "1073428960179",
-    appId: "1:1073428960179:web:c61897786f1d2ba05131c6",
-    measurementId: "G-47T814R2SK"
-  };
+// Your Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5oq9fyPeoo8jVU-N07gYhjt2kFEBGqA8",
+  authDomain: "arc-by-insight.firebaseapp.com",
+  projectId: "arc-by-insight",
+  storageBucket: "arc-by-insight.appspot.com",
+  messagingSenderId: "1073428960179",
+  appId: "1:1073428960179:web:c61897786f1d2ba05131c6",
+  measurementId: "G-47T814R2SK"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const messaging = getMessaging(app);
+// Initialize Firebase with the configuration
+const app = initializeApp(firebaseConfig);
 
+// Retrieve an instance of Firebase Messaging
+const messaging = getMessaging(app);
+
+// Event listener for handling background messages
 onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
- console.log(payload);
-  const notificationTitle = 'Background Message Title';
+  console.log('[firebase-messaging-sw.js] Received background message:', payload);
+
+  // Customize the notification options
+  const notificationTitle = 'New message';
   const notificationOptions = {
-    body: 'Background Message body.',
+    body: payload.data.body,
+    icon: 'icon.png', // Customize with the path to your app's icon
   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  // Show the notification
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });

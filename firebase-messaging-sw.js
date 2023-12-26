0745dd26c8338a8cbd3c9055726c030e7c8cddc3
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-26 00:47:24 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-importScripts('/__/firebase/10.5.0/firebase-app-compat.js');
-importScripts('/__/firebase/10.5.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js');
 
 // Your Firebase project configuration
 const firebaseConfig = {

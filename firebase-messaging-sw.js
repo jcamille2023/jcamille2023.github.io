795a8050ef569717a30cabb759f66228a38b5a87
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-25 19:29:41 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,34 @@
- 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getMessaging, getToken, onMessage, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC5oq9fyPeoo8jVU-N07gYhjt2kFEBGqA8",
+    authDomain: "arc-by-insight.firebaseapp.com",
+    databaseURL: "https://arc-by-insight-default-rtdb.firebaseio.com",
+    projectId: "arc-by-insight",
+    storageBucket: "arc-by-insight.appspot.com",
+    messagingSenderId: "1073428960179",
+    appId: "1:1073428960179:web:c61897786f1d2ba05131c6",
+    measurementId: "G-47T814R2SK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const messaging = getMessaging(app);
+
+onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+ console.log(payload);
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});

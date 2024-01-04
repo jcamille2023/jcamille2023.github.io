ce93fa49ed123f26c5a1a5ab5a801798c06af2b9
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2024-01-04 03:53:04 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,11 +12,13 @@ firebase.initializeApp({
   measurementId: "G-47T814R2SK"
 });
 
+var channel_id;
 // Initialize Firebase with the configuration
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
+  channel_id = payload.data.channelId;
   const notificationTitle = payload.data.displayName + ' in ' + payload.data.channel_name;
   const notificationOptions = {
     body: payload.data.content,
@@ -28,3 +30,24 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
+
+
+self.onnotificationclick = (event) => {
+  console.log("On notification click: ", event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === "/arc/channel?channel_id=${channel_id}" && "focus" in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow("/arc/channel?channel_id=${channel_id}");
+      }),
+  );
+};
